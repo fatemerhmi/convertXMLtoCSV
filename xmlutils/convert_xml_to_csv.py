eb5c8f55e8f9896bc13de930d195edcc826491f2
Fatemeh Rahimi <fateme.rhmi@gmail.com>
2020-06-10 12:38:15 -0300

    create csv with attribute added

--- xmlutils/convert_xml_to_csv.py
+++ xmlutils/convert_xml_to_csv.py
@@ -3,6 +3,7 @@ import re
 import os
 import pandas as pd
 import click
+import collections
 
 # Enable click
 os.environ['LC_ALL'] = 'C.UTF-8'
@@ -19,27 +20,41 @@ def getfilename(xml_file):
 @click.option('-o','--output-dir', type=click.Path(exists=True, resolve_path=True), default ="./" , 
             help='Path to the output directory.')
 @click.option('-c', "--column-based", type=str, default = "attrib" ,
-            help="'attrib' for setting attributes as columns, 'tag' for setting tags as columns" )
-def main(xml_file,output_dir, column_based):
-    print(output_dir)
-    tree = ET.parse(xml_file)
-    root = tree.getroot()
+            help="'attrib' for setting attributes as columns, 'tag' for setting tags as columns." )
+# sample 1: -c attrib
+@click.option('-a', '--attrib', type = str, default = "" ,
+            help = "The attribute name that its value is going to be the column names." )
+# sample 1: -a name
+def main(xml_file,output_dir, column_based, attrib):
+    try:
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+    except FileNotFoundError as fnf_error:
+        raise RuntimeError(fnf_error)
 
     ########## Finding column names ############
     columns = []
     if column_based == "attrib":
         for child in root:
-            print(child.attrib)
+            for item in child:
+                columns.append(item.attrib[attrib])
+            break
     elif column_based == "tag":
             print(child.tag)
     else:
-        raise RuntimeError("Only attrib and tag are valid values for --column-based option.")
-    topic_df = pd.DataFrame(columns = columns)
-    # for child in root:
-    #     no = child.attrib['number']
-    #     topic_df = topic_df.append({child.tag : child.attrib['number'], child[0].tag : child[0].text ,child[1].tag : child[1].text , child[2].tag : child[2].text},ignore_index=True)
+        raise Exception("Only attrib and tag are valid values for --column-based option.")
+    
+    print("column names are:", columns)
 
-    # topic_df.to_csv(getfilename(xml_file) + ".csv", index=False) 
+    ########### Create csv file #########
+    xml_df = pd.DataFrame(columns = columns)
+    for child in root:
+        row = collections.OrderedDict()
+        for item in child:
+            row[item.attrib[attrib]: item.text]
+        xml_df = xml_df.append(row,ignore_index=True)
+
+    topic_df.to_csv(getfilename(xml_file) + ".csv", index=False) 
 
 if __name__ == "__main__":
     main()
